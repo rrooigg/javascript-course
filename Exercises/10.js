72b52f96e46c4fe7e3bcedef1c68bba07c7fce45
rrooigg <devnadifa@gmail.com>
2026-09-01 13:46:50 +0300

    exercise 10 -part 2; style calculator & add error message to cost calculator

--- Exercises/10.js
+++ Exercises/10.js
@@ -5,8 +5,15 @@ function isToggled(selector) {
   if(buttonElement.classList.contains('is-toggled')){
     buttonElement.classList.remove('is-toggled');
   } else {
+    turnOffPreviousButton();
     buttonElement.classList.add('is-toggled');
   }
 }
-  
+//turn-off any toggled button
+function turnOffPreviousButton() {
+  const previousButton = document.querySelector('.is-toggled');
+  if(previousButton) {
+    previousButton.classList.remove('is-toggled');
+  }
+}
 
